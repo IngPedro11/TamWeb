2e28dd07be8c60cd9c04bf31227276749d694568
suarez86 <p.suarez-acosta@hzdr.de>
2026-09-21 23:45:44 +0200

    test

--- js/main.js
+++ js/main.js
@@ -9,18 +9,3 @@ sceneManager.start(
     new SunsetScene(sceneManager)
 );
 
-.tree {
-    position: absolute;
-
-    left: 0;
-    bottom: 100%;
-
-    width: 0;
-    height: 0;
-
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
-    border-bottom: var(--tree-height) solid var(--tree);
-
-    transform: translateX(-50%);
-}
--- js/scenes/SunsetScene.js
+++ js/scenes/SunsetScene.js
@@ -2,42 +2,66 @@ export default class SunsetScene {
 
     constructor(sceneManager) {
         this.sceneManager = sceneManager;
+        this.container = null;
     }
 
     start() {
         this.createLandscape();
-        this.createSun();
-        this.createStars();
-        this.setupInteraction();
+        this.createTrees();
     }
 
     createLandscape() {
-        // cielo
-        // montañas
-        // árboles
-        // tierra
+        this.container = document.createElement("div");
+        this.container.className = "sunset-scene";
+
+        this.container.innerHTML = `
+            <div class="sky"></div>
+
+            <div class="mountains-far"></div>
+
+            <div class="mountains-near"></div>
+
+            <div class="ground"></div>
+        `;
+
+        document
+            .getElementById("app")
+            .appendChild(this.container);
     }
 
-    createSun() {
-        // crear sol
-    }
+    createTrees() {
 
-    createStars() {
-        // crear estrellas
-    }
+        const ground = this.container.querySelector(".ground");
 
-    setupInteraction() {
-        // arrastrar sol
-    }
+        const treePositions = [
+            2, 6, 11, 17, 24,
+            76, 83, 89, 94, 98
+        ];
 
-    update() {
-        // calcular posición
-        // actualizar colores
-        // actualizar estrellas
-        // actualizar sol
+        treePositions.forEach((position, index) => {
+
+            const tree = document.createElement("div");
+
+            tree.className = "tree";
+
+            tree.style.left = `${position}%`;
+
+            const distanceFromCenter =
+                Math.abs(position - 50);
+
+            const height =
+                25 + distanceFromCenter * 0.8;
+
+            tree.style.setProperty(
+                "--tree-height",
+                `${height}px`
+            );
+
+            ground.appendChild(tree);
+        });
     }
 
     destroy() {
-        // limpiar todo
+        this.container?.remove();
     }
 }
